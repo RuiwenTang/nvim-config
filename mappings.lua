742578f091827d6054c31aa37b9f5944a402c665
tangruiwen.mmh1103 <tangruiwen.mmh1103@bytedance.com>
2023-02-05 17:40:22 +0800

    append clangd keybindings

--- mappings.lua
+++ mappings.lua
@@ -3,6 +3,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "command mode", opts = { nowait = true } },
+    ["<leader>gh"] = { "<cmd> ClangdSwitchSourceHeader <CR>", "switch between header and source" },
   },
 }
 
