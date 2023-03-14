8066b52fa55497e99384dc44c2d7812be1c97d99
tangruiwen.mmh1103 <tangruiwen.mmh1103@bytedance.com>
2023-03-14 23:15:47 +0800

    Change terminal mapping and enable dap

---
+++ plugins/core.lua
@@ -0,0 +1,10 @@
+
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    lazy = false,
+    opts = {
+      open_mapping = [[<c-\>]],
+    }
+  }
+}
--- plugins/user.lua
+++ plugins/user.lua
@@ -28,7 +28,7 @@ return {
             -- Hover actions
             vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
             -- Code action groups
-            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+            vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
           end,
         },
       })
@@ -42,6 +42,7 @@ return {
       local ndp = require('nvim-dap-projects')
 
       ndp.search_project_config()
-    end
+    end,
+    lazy = false,
   }
 }
