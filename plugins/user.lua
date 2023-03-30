1cb6638a04240a51f789488e7a355eae229a1f33
tangruiwen.mmh1103 <tangruiwen.mmh1103@bytedance.com>
2023-03-30 16:56:32 +0800

    Add mini.move

--- plugins/user.lua
+++ plugins/user.lua
@@ -33,5 +33,30 @@ return {
       -- code
       require('one_monokai').setup()
     end
-  }
+  },
+  -- move
+  {
+    'echasnovski/mini.move',
+    version = '*',
+    event = 'BufRead',
+    config = function ()
+      -- code
+      require('mini.move').setup({
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+          -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+          left = '<S-left>',
+          right = '<S-right>',
+          down = '<S-down>',
+          up = '<S-up>',
+
+          -- Move current line in Normal mode
+          line_left = '<S-left>',
+          line_right = '<S-right>',
+          line_down = '<S-down>',
+          line_up = '<S-up>',
+        },
+      })
+    end
+  },
 }
