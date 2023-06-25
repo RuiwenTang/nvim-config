9d54ecb3c9a779c665fc219d5bcd4fd4e195eae1
RuiwenTang <tangruiwen1989@gmail.com>
2023-06-25 10:42:31 +0800

    Add flash.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -70,5 +70,37 @@ return {
       -- code
       require('trouble').setup()
     end
+  },
+  -- flash.nvim
+  {
+    'folke/flash.nvim',
+    event = "VeryLazy",
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          -- default options: exact mode, multi window, all directions, with a backdrop
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+    },
   }
 }
