f7d7825f7afc5fc5c7082853f45802bc2a669caa
RuiwenTang <tangruiwen1989@gmail.com>
2023-09-19 09:53:49 +0800

    add symbol usage plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -103,6 +103,13 @@ return {
       },
     },
   },
+  {
+  'Wansmer/symbol-usage.nvim',
+    event = 'LspAttach', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+    config = function()
+      require('symbol-usage').setup()
+    end
+  },
   -- catppuccin
   {
     "catppuccin/nvim",
